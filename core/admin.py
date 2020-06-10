75983683a75f2d4f58b2b361956a42bf1d0d9f24
David Israel <davidisrael.rj@gmail.com>
2020-06-10 05:57:12 -0300

    Foi criado a função de editar e excluir

--- core/admin.py
+++ core/admin.py
@@ -4,7 +4,7 @@ from core.models import Evento
 
 #mostrando o titulo do evento , data_evento, data_criação
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_display = ('id', 'titulo', 'data_evento', 'data_criacao')
     list_filter = ('titulo', 'data_evento',)
 
 admin.site.register(Evento, EventoAdmin)
